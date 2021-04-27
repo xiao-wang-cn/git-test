3a0bef27b9c72bf43beb650f7ce11119bc0c5554
wei <wxwwxw@qq.com>
2021-04-27 09:38:20 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author xiao-wang
+ * @create 2021-04-27 9:34
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
